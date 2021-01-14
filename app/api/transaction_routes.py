03548a0c7abce50414daa8880a4c4bdb39dba514
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-01-13 18:46:26 -0600

    finished transaction route

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from app.models import User, db, Transaction
 from app.forms.transaction_form import TransactionForm
+from flask_login import current_user
 
 transaction_routes = Blueprint('transaction', __name__)
 
@@ -14,7 +15,7 @@ def getUserTransactions(user_id):
     return {"error": "Not found"}
 
 
-@transaction_routes.route('/buy')
+@transaction_routes.route('/buy', methods=["POST"])
 def newTransaction():
     form = TransactionForm()
     form['csrf_token'].data = request.cookies['csrf_token']
@@ -25,8 +26,12 @@ def newTransaction():
             amount=form.data['amount'],
             price=form.data['price'],
         )
-        # add error handling if user has not enough money
         user = User.query.get(transaction.user_id)
+        error = ""
+        if user.balance < transaction.amount * transaction.price:
+            error = "insufficient balance to buy this commodity"
+        if error:
+            return {"error": error}, 400
         user.balance -= transaction.amount * transaction.price
         db.session.add(transaction)
         db.session.commit()
@@ -35,9 +40,38 @@ def newTransaction():
     return jsonify(form.errors)
 
 
-@transaction_routes.route('/sell')
+@transaction_routes.route('/sell', methods=["POST"])
 def sellTransaction(id):
-    
+    data = request.get_json()
+    user_id = current_user.get_id()
+    amount = data['amount']
+    price = data['price']
+    commodity_id = data["commodityId"]
+
+    old_transaction = Transaction.query.get(id)
+    error = ""
+    if !old_transaction:
+        error = "Could not find transactions"
+    elif transaction.amount <= 0:
+        error = "Must own at least one share to sell"
+    elif amount > transaction.amount:
+        error = "Can not sell more shares than you own!"
+    if error:
+        return {"error": error}, 400
+    transaction = Transaction(
+        user_id=user_id,
+        commodity_id=commodity_id,
+        amount=amount,
+        price=price,
+    )
+    user = User.query.get(transaction.user_id)
+    user.balance += transaction.amount * transaction.price
+    db.session.add(transaction)
+    db.session.commit()
+    return transaction.to_dict()
+    print(form.errors)
+    return jsonify(form.errors)
+
     # send the transaction id back, and selling amount
     # query database for that transaction
     # create new transation
--- app/seeds/stocks.py
+++ app/seeds/stocks.py
@@ -2,7 +2,8 @@ from app.models import db, Commodity
 
 
 def seed_stocks():
-
+    data = fetch(
+        "https://www.quandl.com/api/v3/datatables/WORLDBANK/WLD_GOLD.csv?ticker=WLD_GOLD&api_key=cw_yb-2sxho6vEFcfaGR")
     general = Commodity(
 
     )
