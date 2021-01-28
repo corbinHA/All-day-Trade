d58d4840f5f85eb26ca4a56722a940caa7702f66
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-01-28 17:17:06 -0600

    heroku error

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from .seeds import seed_commands
 
 from .config import Config
 
-app = Flask(__name__, static_url_path="../react-app/build/static")
+app = Flask(__name__, static_folder="../react-app/build/static")
 
 # Setup login manager
 login = LoginManager(app)
