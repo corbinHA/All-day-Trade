8d9ace897c4e641652f76fc6c23c88bd9546c9b1
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-01-28 17:26:48 -0600

    add build directory

---
+++ react-app/build/precache-manifest.37d1ce753b9ac3043cf21af986b5337c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9bb91d83497d1bf856859a0b8283ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1a5f47801a1c4e7442",
+    "url": "/static/css/main.7dbd6966.chunk.css"
+  },
+  {
+    "revision": "21ef3e9be6bda81788cf",
+    "url": "/static/js/2.97481e46.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.97481e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1a5f47801a1c4e7442",
+    "url": "/static/js/main.2b8385dd.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5b1596287cf02f6b7f18556ba39b8d3f",
+    "url": "/static/media/LPL.5b159628.png"
+  },
+  {
+    "revision": "7063ea2d58a56bfbe80d1e0a053529ab",
+    "url": "/static/media/ingot.7063ea2d.png"
+  },
+  {
+    "revision": "247893bce468fab4cc915dd6d274d6cd",
+    "url": "/static/media/loginPic.247893bc.png"
+  },
+  {
+    "revision": "4b3c3a9a2b7563be0ee3de6a929d8aa0",
+    "url": "/static/media/signup.4b3c3a9a.png"
+  }
+]);
